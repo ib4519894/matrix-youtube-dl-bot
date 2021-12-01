930739b673f905d2f3fc72f2ff57b16fca2d0045
krazykirby99999 <krazykirby99999@gmail.com>
2021-12-01 10:53:09 -0600

    Add youtube link parser function

---
+++ src/parser.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def parse(str: input_text) -> List[str]:
+    urls = []
+    for part in input_text.split():
+        match = re.findall(r'(https?://)?(www\.)?''(youtube|youtu|youtube-nocookie)\.(com|be)/''(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})', part)
+        if list(match):
+            urls.append(f"https://www.youtube.com/watch?v={match[0][5]}")
