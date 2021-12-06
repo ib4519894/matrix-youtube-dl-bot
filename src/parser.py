9fc759e157af8c8e3037ae9c718bf17bf2ec07d5
ib4519894 <ib4519894@lpssonline.com>
2021-12-06 16:11:33 +0000

    Fix typo in parser parameters

--- src/parser.py
+++ src/parser.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def parse(str: input_text) -> List[str]:
+def parse(input_text: str) -> List[str]:
     urls = []
     for part in input_text.split():
         match = re.findall(r'(https?://)?(www\.)?''(youtube|youtu|youtube-nocookie)\.(com|be)/''(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})', part)
