6d42a6e09c37d3e75cf5f34ce7dd820263b23d13
ib4519894 <ib4519894@lpssonline.com>
2021-12-07 15:55:55 +0000

    Create and run bot in main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main() -> None:
-    ...
+import sys
+import simplematrixbotlib as botlib
+import src
 
-if ___name__ == '__main__':
+def main() -> None:
+    creds = botlib.Creds(homeserver=sys.argv[1], username=sys.argv[2], password=sys.argv[3])
+    bot = src.Bot(creds)
+    bot.run()
+
+if __name__ == '__main__':
     main()
