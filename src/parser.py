858963e03b734dba3a916379ba3fd08c481760a4
ib4519894 <ib4519894@lpssonline.com>
2021-12-13 15:57:23 +0000

    Modify parse function to return a str instead of a list of str

--- src/parser.py
+++ src/parser.py
@@ -1,9 +1,7 @@
 from typing import List
 
-def parse(input_text: str) -> List[str]:
-    urls = []
+def parse(input_text: str) -> str:
     for part in input_text.split():
         match = re.findall(r'(https?://)?(www\.)?''(youtube|youtu|youtube-nocookie)\.(com|be)/''(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})', part)
         if list(match):
-            urls.append(f"https://www.youtube.com/watch?v={match[0][5]}")
-    return urls
+            return f"https://www.youtube.com/watch?v={match[0][5]}"
