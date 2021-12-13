70457e06e6dfd52be9e3e75d89a6231398bc45c2
ib4519894 <ib4519894@lpssonline.com>
2021-12-13 16:11:30 +0000

    Add youtube-dl options to main.py

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import sys
 import simplematrixbotlib as botlib
 import src
 
+YOUTUBE_DL_OPTS = {
+    'max_filesize' : 50
+}
+
 def main() -> None:
     creds = botlib.Creds(homeserver=sys.argv[1], username=sys.argv[2], password=sys.argv[3])
-    bot = src.Bot(creds)
+    bot = src.Bot(creds, youtube_dl_options=YOUTUBE_DL_OPTS)
     bot.run()
 
 if __name__ == '__main__':
